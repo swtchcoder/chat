24344d64e5e4d1f332fdc19876c0026e0f4997d0
switchcodeur <switchcodeur@switchcodeur.com>
2026-01-12 14:53:37 +0100

    server Added sqlite users schema

--- server/main.go
+++ server/main.go
@@ -7,12 +7,33 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+const addr = ":3333"
+var db *sql.DB
+
 func main() {
-	db, err := sql.Open("sqlite3", "database.db")
+	var err error
+	var schema *sql.Stmt
+
+	db, err = sql.Open("sqlite3", "database.db")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	err = http.ListenAndServe(":3333", nil)
+	schema, err = db.Prepare(`
+CREATE TABLE IF NOT EXISTS users (
+	username TEXT PRIMARY KEY,
+	password TEXT NOT NULL,
+	key TEXT
+);
+	`)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_, err = schema.Exec()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
